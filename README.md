dd869e213820fda34f0326316d23cce1af27ce53
Federico J. Lee <fede.in.lakech@gmail.com>
2022-09-05 14:43:56 -0300

    primer commit

--- README.md
+++
@@ -1,9 +0,0 @@
-# clase-02
-
-## ejercicio 02
-* Subir el ejercicio a Netlify o github pages
-* Agregar bootstrap
-* Cargar un JS y mostrar por consola el objeto document de JS.
-
-## opcional
-* Usar algun elemento de Bootstrap
--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-// A partir de ES6
-let myVar = 0;
-var myVar2 = 1;
-
 const myDom = document;
-
 console.log(myDom);
+
+const video = myDom.getElementById("video");
+
+const myVideo = `<li class="list-group-item"><video width="320" height="240" controls> <source src="./images/LFC.mp4"></video></li>`;
+
+const cantidad = prompt("¿Cuántas veces querés que aparezca el video?");
+
+for (let i = 1; i <= cantidad; i++) {
+  video.innerHTML += myVideo;
+}
